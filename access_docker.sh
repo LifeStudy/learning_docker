7dd7089481fd5aa71e0ec7af49aa1135de7d3087
jobino <you@example.com>
2018-08-18 23:50:54 -0300

    shell file created for access docker

---
+++ access_docker.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+docker run \
+        -ti \
+        --rm \
+        -e DISPLAY=$DISPLAY \
+        -v /tmp/.X11-unix:/tmp/.X11-unix \
+        -v $(pwd):/home/developer \
+        --privileged \
+        --device /dev/ttyUSB0 \
+        --device /dev/ttyUSB1 \
+        --device /dev/ttyUSB2 \
+        --device /dev/ttyUSB3 \
+        --device /dev/ttyUSB4 \
+        --device /dev/ttyUSB5 \
+        --device /dev/ttyUSB6 \
+        --device /dev/ttyUSB7 \
+        -v /dev/bus/usb:/dev/bus/usb \
+        react-native:js bash
